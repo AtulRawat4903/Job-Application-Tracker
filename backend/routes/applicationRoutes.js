4e78fce5b86e9a70efb63be67c0fb73d90ebb44a
Atul Rawat <atulrawat104903@gmail.com>
2026-06-11 16:41:48 +0530

    Add CRUD API with MongoDB Atlas

--- backend/routes/applicationRoutes.js
+++ backend/routes/applicationRoutes.js
@@ -3,7 +3,6 @@ const router = express.Router();
 
 const Application = require("../models/Application");
 
-
 // CREATE APPLICATION
 router.post("/", async (req, res) => {
     try {
@@ -17,13 +16,27 @@ router.post("/", async (req, res) => {
     }
 });
 
-
 // GET ALL APPLICATIONS
 router.get("/", async (req, res) => {
     try {
         const applications = await Application.find();
 
         res.status(200).json(applications);
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        });
+    }
+});
+
+// DELETE APPLICATION
+router.delete("/:id", async (req, res) => {
+    try {
+        await Application.findByIdAndDelete(req.params.id);
+
+        res.status(200).json({
+            message: "Application deleted"
+        });
     } catch (error) {
         res.status(500).json({
             message: error.message
